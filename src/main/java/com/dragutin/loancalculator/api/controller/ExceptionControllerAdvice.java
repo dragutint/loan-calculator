dadcceaedb3c2468f9ca0fe3e41ff6d0aec70c54
Dragutin Todorovic <duda.todorovic.97@gmail.com>
2022-06-26 15:16:56 +0200

    Implemented error handling on API layer

---
+++ src/main/java/com/dragutin/loancalculator/api/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,53 @@
+package com.dragutin.loancalculator.api.controller;
+
+import com.dragutin.loancalculator.api.domain.ApiError;
+import com.dragutin.loancalculator.bl.exception.InvalidCalculationParameterException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.UUID;
+
+
+@Log4j2
+@ControllerAdvice
+public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        BindingResult result = ex.getBindingResult();
+        ApiError apiError = new ApiError(UUID.randomUUID().toString(), HttpStatus.BAD_REQUEST, "Validation error", ex);
+        apiError.addValidationErrors(result.getFieldErrors());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+
+    @ResponseBody
+    @ExceptionHandler(InvalidCalculationParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handle(InvalidCalculationParameterException e) {
+        ApiError apiError = new ApiError(UUID.randomUUID().toString(), HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        return ResponseEntity.badRequest().body(apiError);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ApiError> handle(Exception e) {
+        String errorId = UUID.randomUUID().toString();
+        log.error(String.format("%s, errorId: %s, ex: ", e.getClass().getName(), errorId), e);
+
+        ApiError apiError = new ApiError(errorId, HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+}
---
+++ src/main/java/com/dragutin/loancalculator/api/domain/ApiError.java
@@ -0,0 +1,78 @@
+package com.dragutin.loancalculator.api.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ApiError {
+
+    private String errorId;
+
+    private HttpStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    private String debugMessage;
+
+    private List<ApiValidationError> validationErrors;
+
+
+    private ApiError() {
+        timestamp = LocalDateTime.now();
+    }
+
+    public ApiError(String errorId,
+                    HttpStatus status,
+                    String message,
+                    Throwable ex) {
+        this();
+        this.errorId = errorId;
+        this.status = status;
+        this.message = message;
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    private void addValidationError(FieldError fieldError) {
+        addValidationError(new ApiValidationError(fieldError.getObjectName(), fieldError.getField(), fieldError.getRejectedValue(), fieldError.getDefaultMessage()));
+    }
+
+    private void addValidationError(ApiValidationError validationError) {
+        if (validationErrors == null)
+            validationErrors = new ArrayList<>();
+
+        validationErrors.add(validationError);
+    }
+
+    public void addValidationErrors(List<FieldError> fieldErrors) {
+        fieldErrors.forEach(this::addValidationError);
+    }
+
+    @Data
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    static class ApiValidationError {
+        private String object;
+
+        private String field;
+
+        private Object rejectedValue;
+
+        private String message;
+
+    }
+}
