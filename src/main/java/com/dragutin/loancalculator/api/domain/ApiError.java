34c76838dbb23cb056ec8a505837c820b7ec0986
Dragutin Todorovic <duda.todorovic.97@gmail.com>
2022-06-26 20:52:40 +0200

    Remove JSON format for LocalDateTime

--- src/main/java/com/dragutin/loancalculator/api/domain/ApiError.java
+++ src/main/java/com/dragutin/loancalculator/api/domain/ApiError.java
@@ -1,6 +1,5 @@
 package com.dragutin.loancalculator.api.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,7 +21,6 @@ public class ApiError {
 
     private HttpStatus status;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
 
     private String message;
